d437f27b11468261f78adfbb9e78d00c0da0ce9c
sushanek <lev.sushi@gmail.com>
2020-02-22 14:10:22 +0300

    6 задание по личному проекту

--- js/picture.js
+++ js/picture.js
@@ -1,12 +1,20 @@
 'use strict';
 
 (function () {
-  var SUM_PHOTO_ARRAYS = 25;
-
   var pictureInsert = document.querySelector('.pictures');
+
+  // Шаблон для отрисовки изображений
   var pictureTemplate = document.querySelector('#picture')
       .content
       .querySelector('.picture');
+
+  // Куда вставляем ошибку
+  var errorInsert = document.querySelector('main');
+
+  // Обработка ошибки
+  var errorTemplate = document.querySelector('#error')
+      .content
+      .querySelector('.error');
 
   var renderPicture = function (photo) {
     var pictureElement = pictureTemplate.cloneNode(true);
@@ -17,13 +25,31 @@
     return pictureElement;
   };
 
-  var createFragment = function (photoArrays) {
-    var fragment = document.createDocumentFragment();
-    for (var i = 0; i < photoArrays.length; i++) {
-      fragment.appendChild(renderPicture(photoArrays[i]));
-    }
-    return fragment;
+  var renderError = function (message) {
+    var errorElement = errorTemplate.cloneNode(true);
+    errorElement.querySelector('.error__title').textContent = message;
+    errorElement.querySelector('.error__button').textContent = 'Закрыть';
+    return errorElement;
   };
 
-  pictureInsert.appendChild(createFragment(window.createPhotoArrays(SUM_PHOTO_ARRAYS)));
+  var loadHandle = function (photos) {
+    var fragment = document.createDocumentFragment();
+    for (var i = 0; i < photos.length; i++) {
+      fragment.appendChild(renderPicture(photos[i]));
+    }
+    pictureInsert.appendChild(fragment);
+  };
+
+  var errorHandle = function (message) {
+    // Отрисовываем ошибку
+    errorInsert.appendChild(renderError(message));
+
+    // Отрабатываем закрытие
+    var errorWindow = document.querySelector('.error');
+    errorWindow.addEventListener('click', function () {
+      errorInsert.removeChild(errorWindow);
+    });
+  };
+
+  window.load('GET', loadHandle, errorHandle);
 })();
